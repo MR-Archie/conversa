19dbce7f712a5aef2bae7c827a3a7486aba72cce
Archisman Mandal <archi.rider14@gmail.com>
2023-07-29 17:55:22 +0530

    .

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,11 +1,34 @@
-import 'package:conversa/widgets/new_messages.dart';
-import 'package:conversa/widgets/prev_messages.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+import 'package:conversa/widgets/new_messages.dart';
+import 'package:conversa/widgets/prev_messages.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:firebase_messaging/firebase_messaging.dart';
+
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   setUpPushNotification();
+  // }
+
+  // void setUpPushNotification() async {
+  //   final fcm = FirebaseMessaging.instance;
+
+  //   await fcm.requestPermission();
+
+  //   final token = await fcm.getToken();
+
+  //   print("the token is : $token");
+  // }
 
   @override
   Widget build(BuildContext context) {
--- lib/widgets/prev_messages.dart
+++ lib/widgets/prev_messages.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:conversa/widgets/message_bubble.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/material.dart';
 
@@ -7,6 +9,8 @@ class PrevMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authenticatedUser = FirebaseAuth.instance.currentUser!;
+
     return StreamBuilder(
       stream: FirebaseFirestore.instance
           .collection("chats")
@@ -14,7 +18,7 @@ class PrevMessages extends StatelessWidget {
           .snapshots(),
       builder: (ctx, chatSnapshots) {
         if (chatSnapshots.connectionState == ConnectionState.waiting) {
-          return const CircularProgressIndicator();
+          return const Center(child: CircularProgressIndicator());
         }
 
         if (!chatSnapshots.hasData || chatSnapshots.data!.docs.isEmpty) {
@@ -41,7 +45,25 @@ class PrevMessages extends StatelessWidget {
           itemCount: loadedMessages.length,
           reverse: true,
           itemBuilder: (ctx, index) {
-            return Text(loadedMessages[index].data()['text']);
+            final chatMessage = loadedMessages[index].data();
+            final nextChatMessage = (index + 1) < loadedMessages.length
+                ? loadedMessages[index + 1].data()
+                : null;
+            final currentUserId = chatMessage['userId'];
+            final nextUserId =
+                (nextChatMessage != null) ? nextChatMessage['userId'] : null;
+            final isNextUserSame = currentUserId == nextUserId;
+            if (isNextUserSame) {
+              return MessageBubble.next(
+                  message: chatMessage['text'],
+                  isMe: currentUserId == authenticatedUser.uid);
+            } else {
+              return MessageBubble.first(
+                  userImage: chatMessage['userImage'],
+                  username: chatMessage['userName'],
+                  message: chatMessage['text'],
+                  isMe: currentUserId == authenticatedUser.uid);
+            }
           },
         );
       },
